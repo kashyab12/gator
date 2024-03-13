e93bafa9eeab001895de584f21747dc9bf382238
kashyaba <iamkashyab@gmail.com>
2024-03-13 01:50:37 -0700

    ingest env vars and port

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+const (
+	DefaultPort = "8080"
+)
 
 func main() {
+	if loadErr := godotenv.Load(); loadErr != nil {
+		log.Fatalln(loadErr)
+	}
+	if serverPort := os.Getenv("PORT"); serverPort == "" {
+		serverPort = DefaultPort
+	}
 	fmt.Println("gator gator")
 }
