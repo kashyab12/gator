329a9e130fc6e8560098a223ba8f93dfbf6ef3d2
kashyaba <iamkashyab@gmail.com>
2024-03-15 23:48:18 -0700

    define the users table within migration

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -1,5 +1,10 @@
 -- +goose Up
-CREATE TABLE ...
+CREATE TABLE users (
+    uuid INTEGER PRIMARY KEY,
+    created_at TIMESTAM NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    name TEXT NOT NULL
+)
 
 -- +goose Down
 DROP TABLE users;
