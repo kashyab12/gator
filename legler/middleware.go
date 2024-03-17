3ffa333df11272224af7629b30260af95a33d34e
kashyaba <iamkashyab@gmail.com>
2024-03-17 00:13:24 -0700

    Defining the auth middleware :), do

--- legler/middleware.go
+++ legler/middleware.go
@@ -1,6 +1,12 @@
 package legler
 
-import "net/http"
+import (
+	"github.com/kashyab12/gator/internal/database"
+	"net/http"
+	"strings"
+)
+
+type AuthedHandler func(w http.ResponseWriter, r *http.Request, user database.User)
 
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
@@ -13,4 +19,21 @@ func CorsMiddleware(next http.Handler) http.Handler {
 		}
 		next.ServeHTTP(writer, request)
 	})
+}
+
+func (config *ApiConfig) AuthMiddleware(authHandler AuthedHandler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if authHeader := r.Header.Get("Authorization"); authHeader == "" {
+			_ = RespondWithError(w, http.StatusUnauthorized, "Please provide ApiKey")
+		} else if splitAuthHeader := strings.Split(authHeader, "ApiKey "); len(splitAuthHeader) < 2 {
+			_ = RespondWithError(w, http.StatusUnauthorized, "Please provide ApiKey in format Authorization: ApiKey <key>")
+		} else {
+			apiKey := splitAuthHeader[1]
+			if queryUser, queryErr := config.DB.GetUser(r.Context(), apiKey); queryErr != nil {
+				_ = RespondWithError(w, http.StatusInternalServerError, queryErr.Error())
+			} else {
+				authHandler(w, r, queryUser)
+			}
+		}
+	}
 }
