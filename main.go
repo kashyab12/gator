06a6c38212f0a5e35bd5fdb479a95f98a66af587
kashyaba <iamkashyab@gmail.com>
2024-03-16 18:29:47 -0700

    chi based routing :D

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	chi "github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
+	"github.com/kashyab12/gator/internal/database"
 	"github.com/kashyab12/gator/legler"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
 	"os"
@@ -14,10 +18,19 @@ func main() {
 		log.Fatalln(loadErr)
 	}
 	serverPort := os.Getenv("PORT")
-	// TODO: add chi and define separate routers and mount
-	appRouter := http.NewServeMux()
-	appRouter.HandleFunc("/v1/readiness", legler.GetReadinessLegler)
-	appRouter.HandleFunc("/v1/err", legler.GetErrorLegler)
+	dbURL := os.Getenv("DB_CONN")
+	db, openErr := sql.Open("postgres", dbURL)
+	if openErr != nil {
+		fmt.Println("Unable to open psql db connection")
+	}
+	dbQueries := database.New(db)
+	handlerConfig := legler.ApiConfig{
+		DB: dbQueries,
+	}
+	appRouter := chi.NewRouter()
+	appRouter.Get("/v1/readiness", legler.GetReadinessLegler)
+	appRouter.Get("/v1/err", legler.GetErrorLegler)
+	appRouter.Post("/v1/users", handlerConfig.PostUsersLegler)
 	corsMux := legler.CorsMiddleware(appRouter)
 	server := http.Server{
 		Handler: corsMux,
